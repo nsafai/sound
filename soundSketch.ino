6c1979644af5bb0ae602e0fe8e7e6769d885355d
Nicolai Safai <nicolaisafai@gmail.com>
2019-03-09 13:38:54 -0800

    visualizer mvp inside loop() complete

--- soundSketch.ino
+++ soundSketch.ino
@@ -6,6 +6,8 @@
 #define NUM_LEDS 256
 #define DATA_PIN 6
 
+const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
+unsigned int sample;
 
  Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, DATA_PIN,
   NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
@@ -17,26 +19,53 @@ const uint16_t colors[] = {
 
 void setup() {
   delay(3000);
+  Serial.begin(9600);
   matrix.begin();
 }
 
-
-void printRainbowText(String text) {
+void loop() {
+//  printRainbowText("Hell yeah !");
+//  getMicData();
+  int micValue = getMicData() * 100 / 3 - 1;
+  Serial.println(micValue);
   matrix.fillScreen(0);
-  matrix.fillRect(0, 0, 8, 3, colors[0]); 
-  matrix.fillRect(0, 3, 8, 2, colors[1]); 
-  matrix.fillRect(0, 5, 8, 3, colors[2]); 
+  matrix.fillRect(0, 0, micValue/1.25, 3, matrix.Color(0, 130, 0)); 
+  matrix.fillRect(0, 3, micValue, 2, matrix.Color(130, 0, 0)); 
+  matrix.fillRect(0, 5, micValue/1.5, 3, matrix.Color(0, 0, 130)); 
   matrix.show();
 }
 
-void loop() {
-  printRainbowText("Hell yeah !");
-//  rastaFlag();
-  delay(50);
-}
+//void drawVisualizer {
+// 
+//}
 
-// where did we end: need to put loop inside of printRainbowText function
-// use touch capacitor to change delay or word
-// use switch to turn off
+float getMicData() {
+  unsigned long startMillis= millis();  // Start of sample window
+  unsigned int peakToPeak = 0;   // peak-to-peak level
+  
+  unsigned int signalMax = 0;
+  unsigned int signalMin = 1024;
+  
+  // collect data for 50 mS
+  while (millis() - startMillis < sampleWindow)
+  {
+    sample = analogRead(0);
+    if (sample < 1024)  // toss out spurious readings
+    {
+       if (sample > signalMax)
+       {
+          signalMax = sample;  // save just the max levels
+       }
+       else if (sample < signalMin)
+       {
+          signalMin = sample;  // save just the min levels
+       }
+    }
+  }
+  peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+  double volts = (peakToPeak * 5.0) / 1024;  // convert to volts
+
+  return(volts);
+}
 
 
